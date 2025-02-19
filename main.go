006113e226020c7b93fb7db38198e6c1f55a190d
NatsuiroGinga <duyueyeweiliang@gmail.com>
2025-02-19 16:37:05 +0800

    MyDocker-V0.2 实现独立命名空间

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
@@ -18,10 +19,13 @@ func main() {
 
 func run() {
 	cmd := exec.Command(os.Args[2])
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		return
+		panic(err)
 	}
 }
